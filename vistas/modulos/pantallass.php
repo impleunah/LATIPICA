2b77e42fa60fb3644d2dfa9230c740e93f7ee370
impleunah <impleunah@gmail.com>
2020-02-21 01:38:40 -0600

    Revert "aaa"
    This reverts commit 474452d43eb47c5d4f648647cba8c7799df191df.

--- vistas/modulos/pantallass.php
+++ vistas/modulos/pantallass.php
@@ -1,6 +1,14 @@
-<?php
-  include "modelos/conexion2.php";
+<<?php
+$servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "latipica1";
   
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+    if($conn->connect_error){
+      die("Conexión fallida: ".$conn->connect_error);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -101,7 +109,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 </table>
     </form>
     </div>
-  </table>
  </div>
 
 </div>
