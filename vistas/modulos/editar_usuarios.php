f3dec599ca253bba7ad564ae4ebb3fd4a32e5a05
impleunah <impleunah@gmail.com>
2020-02-21 22:24:00 -0600

    cambios

--- vistas/modulos/editar_usuarios.php
+++ vistas/modulos/editar_usuarios.php
@@ -20,16 +20,16 @@
         
     
       }
-
-if (isset($_POST["enviar"])){
-
-//ModificarUsuario($_POST['Nombre_usuario'], $_POST['id_rol'], $_POST['correo_electronico'],$_POST['estado_usario'], $_POST['id_usuario']);
- 
-function ModificarUsuario($nombre, $idrol, $correo,$estado, $id)
+      function ModificarUsuario1($nombre, $idrol, $correo,$estado, $id)
 {
   include "../../modelos/conexion2.php";
 
-  $sentencia="UPDATE tbl_usuario SET Nombre_usuario='".$nombre."', id_rol= '".$idrol."', correo_electronico='".$correo."',estado_usuario='".$estado."' WHERE id_usuario='".$id."' ";
+  $sentencia="UPDATE tbl_usuario SET Nombre_Usuario='".$nombre."', id_rol= '".$idrol."', correo_electronico='".$correo."',estado_usuario='".$estado."' WHERE id_usuario='".$id."' ";
+  $objeto="Editar Usuario";
+  $accion="Modifico"; 
+  $descripcion="Modifico campos de usuario ";
+  
+  $insertarUno=$conn->query("INSERT INTO   tbl_bitacoras(id_usuario,objeto,accion,descripcion,Antes,Despues) VALUES ('$id','$objeto','$accion','$descripcion','($_POST['Nombre_Usuario']).$_POST['Id_rol']. $_POST['correo_electronico'].$_POST['estado_uduario']') '");
   $resultado=$conn->query ($sentencia) or die (mysql_error());
   if($resultado > 0){
    echo "<script>
@@ -45,6 +45,12 @@ function ModificarUsuario($nombre, $idrol, $correo,$estado, $id)
    }
 
   }
+
+if (isset($_POST["enviar"])){
+
+  ModificarUsuario1(strtoupper($_POST['Nombre_Usuario']), $_POST['Id_rol'], $_POST['correo_electronico'],$_POST['estado_uduario'],$_GET['id_usuario']);
+ 
+
 }
 ?>
 
